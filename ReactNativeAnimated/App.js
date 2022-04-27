64f2894bd354293986412ce51e9f74577ccc522c
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2022-04-27 15:14:03 -0700

    reestablishing connection with react native knowledge center...

--- ReactNativeAnimated/App.js
+++ ReactNativeAnimated/App.js
@@ -1,10 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Alert,
+  Button,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hey there beautiful ❤️</Text>
+      <Button
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+        onPress={() => Alert.alert("Simple Button pressed")}
+      />
+      <TouchableHighlight
+        style={styles.submit}
+        onPress={() => Alert.alert("Touchable Highlight pressed")}
+        underlayColor="#212121"
+      >
+        <Text style={styles.submitText}>Submit</Text>
+      </TouchableHighlight>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +33,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  submit: {
+    marginRight: 40,
+    marginLeft: 40,
+    marginTop: 10,
+    padding: 15,
+    backgroundColor: "#68a0cf",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#fff",
+  },
+  submitText: {
+    color: "#fff",
+    textAlign: "center",
   },
 });
